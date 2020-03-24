caa5e310aa4f4a3f20a2936089873c6c032a0cba
dreamml <820914428@qq.com>
2020-03-24 22:42:34 +0800

    fix bug

--- gamecenter/cfg.py
+++ gamecenter/cfg.py
@@ -72,12 +72,6 @@ register('DEFAULT', 'debug', 'true', comment='whether enable debug logging')
 register('DB', 'sql_connection', "mysql://root:123456@localhost/gamecenter",
          comment='Datebase url that xedge itself maintained')
 
-# SDK
-register('SDK', 'cp_game_key', "pg@wowei#!QAZ@WSX",
-         comment='the secret key')
-register('SDK', 'host', "http://fw.woweicm.com",
-         comment='the secret key')
-
 # Mongodb part
 register('MONGODB', 'address', "127.0.0.1:27017", comment='mongo address 1')
 # register('MONGODB', 'username', '', comment='Mongodb username')
